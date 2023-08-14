bf8ad1a05ad4aefd5ae5828db7a23d3d2b90c5f6
natalia fernandez <nata.fernandez77@gmail.com>
2023-08-14 09:00:17 -0300

    creacion del ejercicio 1

---
+++ ejericios.py
@@ -0,0 +1,12 @@
+'''
+Asigne un mensaje a una variable e imprima ese mensaje. Luego cambie el valor de la
+variable a un nuevo mensaje e imprima el nuevo mensaje.
+'''
+var1 = "Hola mundo"
+mensaje = f"El valor de la variable es {var1}"
+print(mensaje)
+
+'''
+Use una variable para representar el nombre de una persona e imprima el mensaje "Hola
+<nombre de la persona>,¿te gustaría aprender algo de Python hoy?".
+'''
